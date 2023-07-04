b979598ed355635d225daeff79daaf3816475125
azratr <134767928+azratr@users.noreply.github.com>
2023-07-04 22:43:34 +0530

    update

--- client/src/actions/AuthAction.js
+++ client/src/actions/AuthAction.js
@@ -1,11 +1,11 @@
 import * as AuthApi from "../api/AuthRequest";
 
-export const logIn = (formData,navigate) => async (dispatch) => {
+export const logIn = (formData) => async (dispatch) => {
   dispatch({ type: "AUTH_START" })
   try {
     const { data } = await AuthApi.logIn(formData);
     dispatch({ type: "AUTH_SUCCESS", data: data });
-    navigate("../home",{replace:true});
+    
   } catch (error) {
     console.log(error);
     dispatch({ type: "AUTH_FAIL" })
@@ -17,7 +17,7 @@ export const signUp = (formData,navigate) => async (dispatch) => {
   try {
     const { data } = await AuthApi.signUp(formData)
     dispatch({ type: "AUTH_SUCCESS", data: data })
-    navigate("../home",{replace:true})
+    
 
   } catch (error) {
     console.log(error);
